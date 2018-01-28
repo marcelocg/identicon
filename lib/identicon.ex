5d5fe1a99d1143a5111361f67e369cd98a25f0ef
Marcelo Goncalves <marcelocg@gmail.com>
2018-01-28 17:19:54 -0300

    Lecture 36

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,22 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Main module for the Identicon application.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
+  Main funcion that will drive our identicon generation pipeline execution
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  @doc """
+  Hashes the input string so we can get a consistent representation of the input
+
+  """
+  def hash_input(input) do
+    
   end
 end
