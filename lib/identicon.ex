038afef2aad36483c3c55800738335270d8e4533
Marcelo Goncalves <marcelocg@gmail.com>
2018-01-29 05:54:47 -0300

    Lecture 39. Create the Image struct to hold the data

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -17,7 +17,9 @@ defmodule Identicon do
 
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
